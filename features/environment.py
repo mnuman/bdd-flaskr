3953b4e6c5f230e61d3151d4ca5a801336fea2c6
Milco Numan <milco.numan@gmail.com>
2016-01-16 19:10:24 +0100

    First commit

---
+++ features/environment.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import tempfile
+
+# add module to syspath
+pwd = os.path.abspath(os.path.dirname(__file__))
+project = os.path.basename(pwd)
+new_path = pwd.strip(project)
+full_path = os.path.join(new_path,'flaskr')
+
+try:
+    from flaskr import app, init_db
+except ImportError:
+    sys.path.append(full_path)
+    from flaskr import app
+
+
+def before_feature(context, feature):
+    context.client = app.test_client()
+
